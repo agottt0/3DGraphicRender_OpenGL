9828bdd4b95c2ecad24f1299fee93f733cfd9a21
agottt0 <838087765@qq.com>
2025-03-25 09:32:38 +0800

    加入三种光源类型，待合并

--- OpenGL/Shader/color_fs.shader
+++ OpenGL/Shader/color_fs.shader
@@ -11,9 +11,17 @@ struct Material
 struct Light
 {
     vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
+
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 in vec3 Normal;
 in vec3 FragPos;
@@ -31,7 +39,7 @@ void main()
     //diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
-
+    //vec3 lightDir = normalize(-light.direction);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
     //计算光源对当前片段实际的漫反射影响。结果值再乘以光的颜色，得到漫反射分量
@@ -41,8 +49,23 @@ void main()
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = 0;
     if(diff > 0.0)
-       spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+
+    //spotlight
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = (light.cutOff - light.outerCutOff);
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    diffuse *= intensity;
+    specular *= intensity;
+
+    //attenuation
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
 
     vec3 result = ambient + diffuse + specular;
     //环境光分量和漫反射分量相加，然后把结果乘以物体的颜色，获得片段输出颜色
--- OpenGL/Source.cpp
+++ OpenGL/Source.cpp
@@ -19,8 +19,8 @@ void processinput(GLFWwindow* window);
 unsigned int loadTexture(char const* path);
 
 // Window size
-const unsigned int SC_WIDTH = 800;
-const unsigned int SC_HEIGHT = 600;
+const unsigned int SC_WIDTH = 1600;
+const unsigned int SC_HEIGHT = 1200;
 
 //camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
@@ -80,6 +80,19 @@ float vertices[] =
 	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
 };// 六个面，每个面两个三角形六个顶点
 
+// positions all containers
+glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+};
 
 int main()
 {
@@ -186,17 +199,22 @@ int main()
 
 		/*render command*/
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f); //use custom color clear screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);         //clear buffer color  
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear buffer color
 
 		LightingShader.use();
-
-		LightingShader.setVec3("light.position", lightPos);
+		LightingShader.setVec3("light.position", camera.Position);
+		LightingShader.setVec3("light.direction", camera.Front);
+		LightingShader.SetFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+		LightingShader.SetFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
 		LightingShader.setVec3("viewPos", camera.Position);
 
 		//light properties
 		LightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-		LightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+		LightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
 		LightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		LightingShader.SetFloat("light.constant", 1.0f);
+		LightingShader.SetFloat("light.linear", 0.09f);
+		LightingShader.SetFloat("light.quadratic", 0.032f);
 
 
 		//materials properties
@@ -226,21 +244,31 @@ int main()
 
 		//render cube
 		glBindVertexArray(cubeVAO); //if we have multiple VAO, need to bind it everytime	
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			LightingShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
+		//LightCubeShader.use();
+		//LightCubeShader.setMat4("projection", projection);
+		//LightCubeShader.setMat4("view", view);
 
-		LightCubeShader.use();
-		LightCubeShader.setMat4("projection", projection);
-		LightCubeShader.setMat4("view", view);
+		//model = glm::mat4(1.0f);
+		//model = glm::translate(model, lightPos);
+		//model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		//LightCubeShader.setMat4("model", model);
 
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-		LightCubeShader.setMat4("model", model);
-
-		glBindVertexArray(lightcubeVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glBindVertexArray(lightcubeVAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		/*check and call events, swap buffers*/
 		glfwSwapBuffers(window); //storage pixel color buffer 
